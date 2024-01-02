207adec215db87560028a42a8e2c88cb9205a51d
蓮 <20379463+juoliantsai@users.noreply.github.com>
2024-01-02 16:35:14 +0800

    update comment

--- gocron/rountine/main.go
+++ gocron/rountine/main.go
@@ -3,15 +3,17 @@ import (
 "fmt"
 "sync"
 )
-
+// 參考資料https://vocus.cc/article/64ff0f16fd8978000191a081
 func main() {
+  // 讓主程式等待所有的Goroutine完成
   var wg sync.WaitGroup
 
+  // 增加計數器的值，表示有多少個Goroutine需要等待
   wg.Add(1)
   go routine1(&wg)
   wg.Add(1)
   go routine2(&wg)
-  
+  // 當計數器的值為零時，Wait()會返回，並允許主程式繼續執行
   wg.Wait()
   fmt.Println("All Goruntines have finished")
 }
@@ -19,6 +21,7 @@ func routine1(wg *sync.WaitGroup){
   for i:=0;i<10;i++{
     fmt.Println("0")
   }
+  // 減少計數器的值，表示一個Goroutine已完成
   defer wg.Done()
 }
 func routine2(wg *sync.WaitGroup){
